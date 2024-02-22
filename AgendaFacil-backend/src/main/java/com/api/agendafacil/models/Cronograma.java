e27f4ffe17f046146d631feafad2899baecf7a52
alcielma luzinete <62629383+Alcielma@users.noreply.github.com>
2024-02-22 12:17:09 -0300

    Update Cronograma.java

--- AgendaFacil-backend/src/main/java/com/api/agendafacil/models/Cronograma.java
+++ AgendaFacil-backend/src/main/java/com/api/agendafacil/models/Cronograma.java
@@ -1,18 +1,22 @@
 package com.api.agendafacil.models;
 
 import java.io.Serializable;
-import java.time.LocalDate;
+import java.util.List;
 import java.util.UUID;
 
+import jakarta.persistence.CollectionTable;
 import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Inheritance;
 import jakarta.persistence.InheritanceType;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Transient;
 
 import com.api.agendafacil.enums.TipoDeConsulta;
@@ -24,31 +28,46 @@ public class Cronograma implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	@Id
+	@Column(name = "cronograma_id")
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private UUID id;
-	
-	@OneToOne
+	@ManyToOne
 	@JoinColumn (name="ubs_id", nullable=false)
 	@JsonIgnore
 	private UBS ubs;
 	
-
-	@Column(nullable=false, length=200)
-	private LocalDate data;
-	@Column(nullable=false, length=200)
-	private TipoDeConsulta tipoConsulta;
+	@ElementCollection
+	@CollectionTable(name = "cronograma_dias_semana", joinColumns = @JoinColumn(name = "cronograma_id"))
+	@Column(name = "dia_semana")
+	private List<String> diasSemana;
+	
+	@ElementCollection
+	@CollectionTable(name = "cronograma_tipos_consulta", joinColumns = @JoinColumn(name = "cronograma_id"))
+	@Enumerated(EnumType.STRING)
+	@Column(name="tipo_consulta")
+	private List<TipoDeConsulta> tiposConsulta;
+	
+	@ElementCollection
+	@CollectionTable(name = "cronograma_vagas", joinColumns = @JoinColumn(name = "cronograma_id"))
+	@Column(name = "vagas")
+	private List<Integer> vagas;
+	
+	//transients
 	
 	@Transient
 	private String nomeUBSTransient;
 	
-	public Cronograma(LocalDate data, TipoDeConsulta tipoConsulta) {
-		this.data=data;
-		this.tipoConsulta=tipoConsulta;
-		
+	public Cronograma(UUID id, UBS ubs, List<String> diasSemana, List<TipoDeConsulta> tiposConsulta,
+			List<Integer> vagas, String nomeUBSTransient) {
+		this.id = id;
+		this.ubs = ubs;
+		this.diasSemana = diasSemana;
+		this.tiposConsulta = tiposConsulta;
+		this.vagas = vagas;
+		this.nomeUBSTransient = nomeUBSTransient;
 	}
-	
+
 	public Cronograma() {
-		
 	}
 	
 	//getter transient
@@ -72,26 +91,38 @@ public class Cronograma implements Serializable{
 	public void setId(UUID id) {
 		this.id = id;
 	}
-	public LocalDate getData() {
-		return data;
+	
+	public List<String> getDiasSemana() {
+		return diasSemana;
 	}
-	public void setDataHora(LocalDate data) {
-		this.data = data;
+
+	public void setDiasSemana(List<String> diasSemana) {
+		this.diasSemana = diasSemana;
+
 	}
-	public TipoDeConsulta getTipoConsulta() {
-		return tipoConsulta;
+
+	public List<TipoDeConsulta> getTiposConsulta() {
+		return tiposConsulta;
 	}
-	public void setTipoConsulta(TipoDeConsulta tipoConsulta) {
-		this.tipoConsulta = tipoConsulta;
+
+	public void setTiposConsulta(List<TipoDeConsulta> tiposConsulta) {
+		this.tiposConsulta = tiposConsulta;
+	}
+
+	public List<Integer> getVagas() {
+		return vagas;
+	}
+
+	public void setVagas(List<Integer> vagas) {
+		this.vagas = vagas;
 	}
 
 	public static long getSerialversionuid() {
 		return serialVersionUID;
 	}
 
-	public void setData(LocalDate data) {
-		this.data = data;
+	public void setNomeUBSTransient(String nomeUBSTransient) {
+		this.nomeUBSTransient = nomeUBSTransient;
 	}
-	
 	
 }
