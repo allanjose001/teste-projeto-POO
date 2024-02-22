090e5c7970e0742d5bf0242cb08833b283ac2601
alcielma luzinete <62629383+Alcielma@users.noreply.github.com>
2024-02-22 12:11:43 -0300

    Update TipoDeConsulta.java
    ADD pediatra

--- AgendaFacil-backend/src/main/java/com/api/agendafacil/enums/TipoDeConsulta.java
+++ AgendaFacil-backend/src/main/java/com/api/agendafacil/enums/TipoDeConsulta.java
@@ -4,6 +4,7 @@ public enum TipoDeConsulta {
 	MEDICO(1),
 	ENFERMEIRO(1),
 	DENTISTA(1);
+	PEDIATRA(1);
 
 	// Criação de um construtor para me dar acesso a quantidade de vagas que tenho disponiveis
 	private int vagasDisponiveis;
